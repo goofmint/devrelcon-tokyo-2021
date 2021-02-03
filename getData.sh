8a6a418a348a2324a1186cdc94948e88fd7d4de7
Atsushi <atsushi@moongift.jp>
2021-02-03 12:39:53 +0900

    Delete images

--- getData.sh
+++ getData.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-curl -L "https://script.google.com/macros/s/AKfycbyp9nZL06tzl3rJ7rstfJR06uG25s_z36b5PlSSjoBauyc9ko7H/exec?name=organizers" -o _data/organizers.json
-curl -L "https://script.google.com/macros/s/AKfycbyp9nZL06tzl3rJ7rstfJR06uG25s_z36b5PlSSjoBauyc9ko7H/exec?name=sessions" -o _data/sessions.json
-curl -L "https://script.google.com/macros/s/AKfycbyp9nZL06tzl3rJ7rstfJR06uG25s_z36b5PlSSjoBauyc9ko7H/exec?name=speakers" -o _data/speakers.json
-curl -L "https://script.google.com/macros/s/AKfycbyp9nZL06tzl3rJ7rstfJR06uG25s_z36b5PlSSjoBauyc9ko7H/exec?name=sponsors" -o _data/sponsors.json
+curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=organizers" -o _data/organizers.json
+curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=sessions" -o _data/sessions.json
+curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=speakers" -o _data/speakers.json
+curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=sponsors" -o _data/sponsors.json
 
 sh image.sh
-ruby speaker.rb
+# ruby speaker.rb
 ruby organizer.rb
-ruby session.rb
+# ruby session.rb
