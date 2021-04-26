8c50be7281e1a5366b6afa36a9ccdcdde976549d
Atsushi <atsushi@moongift.jp>
2021-04-26 22:37:32 +0900

    Add mak

--- image.sh
+++ image.sh
@@ -2,6 +2,8 @@ cd assets/img/organizers/
 for file in *; do cwebp -q 80 "$file" -o "${file%.*}.webp"; done
 cd ../speakers/
 for file in *; do cwebp -q 80 "$file" -o "${file%.*}.webp"; done
+cd ../misc/
+for file in *; do cwebp -q 80 "$file" -o "${file%.*}.webp"; done
 cd ../sponsors
 for file in *; do cwebp -q 80 "$file" -o "${file%.*}.webp"; done
 cd ../blog
