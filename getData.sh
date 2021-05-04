f2be801c1ae808f08922ca54f6405fc908e20c63
Atsushi <atsushi@moongift.jp>
2021-05-04 14:15:40 +0900

    Talk description

--- getData.sh
+++ getData.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=organizers" -o _data/organizers.json
-#curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=sessions" -o _data/sessions.json
+curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=sessions" -o _data/sessions.json
 curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=speakers" -o _data/speakers.json
 curl -L "https://script.google.com/macros/s/AKfycbzlVQe_lE49nDvlt-_0WEqvjuKhIH_Q3WflKC0mpnHEsBYzig8m6cOt/exec?name=sponsors" -o _data/sponsors.json
 
